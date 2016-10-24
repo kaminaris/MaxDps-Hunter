f58282fc543d933e9c29b1a23287dee920c8f73e
Pawel <zacdbg@gmail.com>
2016-10-24 04:14:43 +0200

    v1.3 - legion update

--- main.lua
+++ main.lua
@@ -3,36 +3,52 @@
 
 -- SPELLS
 local _AMurderOfCrows = 131894;
-local _ArcaneShot = 3044;
+
 local _ChimaeraShot = 53209;
 local _KillShot = 53351;
 local _KillShotMM = 157708;
-local _AimedShot = 19434;
+
 local _GlaiveToss = 117050;
-local _Barrage = 120360;
+
 local _SteadyShot = 56641;
-local _CobraShot = 77767;
 local _DireBeast = 120679;
 local _KillCommand = 34026;
 local _BestialWrath = 19574;
 local _FocusFire = 82692;
-local _Stampede = 121818;
 local _RapidFire = 3045;
 local _ExplosiveShot = 53301;
 local _BlackArrow = 3674;
 local _SerpentSting = 87935;
 local _FocusingShot = 163485;
 
+-- NEW
+local _MarkedShot = 185901;
+local _AimedShot = 19434;
+local _Barrage = 120360;
+local _ArcaneShot = 185358;
+local _Sidewinders = 214579;
+local _Trueshot = 193526;
+local _TitansThunder = 207097;
+local _DireFrenzy = 217200;
+local _AspectoftheWild = 193530;
+local _CobraShot = 193455;
+local _Stampede = 201430;
+local _Windburst = 204147;
+
 -- AURAS
 local _ThrillOfTheHunt = 34720;
 local _SteadyFocus = 177668;
 local _Frenzy = 19623;
+local _Vulnerable = 198925;
+local _MarkingTargets = 223138;
+local _LockandLoad = 194595;
+local _HuntersMark = 185987;
 -- local _BestialWrathAura = 19574; the same as spell id
 
 -- costs
 local _ChimaeraShotCost = 35;
 local _AMurderOfCrowsCost = 30;
-local _AimedShotCost = 30;
+local _AimedShotCost = 50;
 local _GlaiveTossCost = 15;
 local _BarrageCost = 60;
 local _DireBeastCost = 0;
@@ -45,14 +61,13 @@ local _ArcaneShotCost = 30;
 local isStampede = false;
 local isMurderofcrows = false;
 local isDireBeast = false;
+local isDireFrenzy = false;
 local isSteadyFocus = false;
 local isBarrage = false;
-local isFocusingShot = false;
+local isSidewinders = false;
 
 -- Flags
-local shouldGainSf = false;
-local _FlagStamp = false;
-local _SteadyShotTime = false;
+local _AimedShotTime = false;
 
 ----------------------------------------------
 -- Pre enable, checking talents
@@ -60,15 +75,16 @@ local _SteadyShotTime = false;
 TDDps_Hunter_CheckTalents = function()
 	isStampede = TD_TalentEnabled('Stampede');
 	isMurderofcrows = TD_TalentEnabled('A Murder of Crows');
-	isDireBeast = TD_TalentEnabled('Dire Beast');
+	isDireFrenzy = TD_TalentEnabled('Dire Frenzy');
 	isSteadyFocus = TD_TalentEnabled('Steady Focus');
 	isBarrage = TD_TalentEnabled('Barrage');
-	isFocusingShot = TD_TalentEnabled('Focusing Shot');
-	_SteadyShotTime = select(4, GetSpellInfo(_SteadyShot));
-	if not _SteadyShotTime then
-		_SteadyShotTime = 2;
+	isSidewinders = TD_TalentEnabled('Sidewinders');
+	_AimedShotTime = select(4, GetSpellInfo(_AimedShot));
+	_AimedShotCost = TD_ExtractTooltip(_AimedShot, FOCUS_COST);
+	if not _AimedShotTime then
+		_AimedShotTime = 2;
 	else
-		_SteadyShotTime = _SteadyShotTime / 1000;
+		_AimedShotTime = _AimedShotTime / 1000;
 	end
 end
 
@@ -95,214 +111,121 @@ end
 -- Main rotation: Beast Mastery
 ----------------------------------------------
 TDDps_Hunter_BeastMastery = function()
-	local timeShift, spellName = TD_EndCast();
+	local timeShift, currentSpell, gcd = TD_EndCast();
+	local focus, focusMax = TDDps_Hunter_Focus(0, timeShift);
 
-	local focus = TDDps_Hunter_Focus(0, timeShift);
-	if spellName == 'Cobra Shot' then
-		focus = focus + 14;
-	end
+	local tt = TD_SpellAvailable(_TitansThunder, timeShift);
+	local amoc = TD_SpellAvailable(_AMurderOfCrows, timeShift);
+	local db, dbCD = TD_SpellAvailable(_DireBeast, timeShift);
+	local df, dfCD = TD_SpellAvailable(_DireFrenzy, timeShift);
+	local kc, kcCD = TD_SpellAvailable(_KillCommand, timeShift + 1);
+	local bw = TD_SpellAvailable(_BestialWrath, timeShift);
+	local stamp = TD_SpellAvailable(_Stampede, timeShift);
+	local aotw = TD_SpellAvailable(_AspectoftheWild, timeShift);
 
-	local toh = TD_Aura(_ThrillOfTheHunt);
-	local sf = TD_Aura(_SteadyFocus);
-	local sf3 = TD_Aura(_SteadyFocus, 3 + timeShift);
-	local bw = TD_Aura(_BestialWrath, timeShift);
-	local frenzy = TDDps_Hunter_PetFrenzy();
-	local stamp, stampCD = TD_SpellAvailable(_Stampede, timeShift);
-	local ff = TD_Aura(_FocusFire);
-	local kk = TD_SpellAvailable(_KillCommand, timeShift + 2);
-	local bwCd = TD_SpellAvailable(_BestialWrath, timeShift);
-	local ks = TD_SpellAvailable(_KillShot, timeShift);
+	TDButton_GlowCooldown(_AMurderOfCrows, isMurderofcrows and amoc);
+	TDButton_GlowCooldown(_Stampede, isStampede and stamp);
+	TDButton_GlowCooldown(_AspectoftheWild, aotw);
+	TDButton_GlowCooldown(_TitansThunder, tt);
 
-	if isStampede then
-		TDButton_GlowCooldown(_Stampede, stamp);
-	end
-
-	if isStampede and stampCD > 265 and frenzy >= 1 and not ff then
-		return _FocusFire;
-	end;
-
-	if not sf and shouldGainSf then
-		shouldGainSf = false;
-		return _CobraShot;
-	end
-
-	if frenzy == 5 and not ff then
-		return _FocusFire;
-	end;
-
-	if bw and kk and not ff and frenzy >= 1 then
-		return _FocusFire;
-	end
-
-	if isMurderofcrows and focus >= _AMurderOfCrowsCost and TD_SpellAvailable(_AMurderOfCrows) then
-		return _AMurderOfCrows;
-	end
-
-	if bwCd and not bw and kk then
+	if bw then
 		return _BestialWrath;
 	end
 
-	if focus >= _KillCommandCost and TD_SpellAvailable(_KillCommand, timeShift) then
-		return _KillCommand;
-	end
-
-	local targetPH = TD_TargetPercentHealth();
-	if targetPH < 0.2 and ks then
-		return _KillShot;
-	end
-
-	if isDireBeast and focus >= _DireBeastCost and TD_SpellAvailable(_DireBeast, timeShift) then
+	if not isDireFrenzy and (db or (dbCD < kcCD and focus < 80 and dbCD < 2)) then
 		return _DireBeast;
 	end
 
-	-- 6. Barrage
-	if isBarrage and focus >= _BarrageCost and TD_SpellAvailable(_Barrage, timeShift) then
-		return _Barrage;
+	if isDireFrenzy and (df or (dfCD < kcCD and focus < 80 and dfCD < 2)) then
+		return _DireFrenzy;
 	end
 
-	if not sf and focus < 80 then
-		shouldGainSf = true;
+	if kcCD < 1 then
+		return _KillCommand;
+	end
+
+	if focus > 80 then
 		return _CobraShot;
+	else
+		return nil;
 	end
-
-	-- 6. Barrage
-	if focus >= 80 then
-		return _ArcaneShot;
-	end
-
-	return _CobraShot;
 end
 
 ----------------------------------------------
 -- Main rotation: Marksmanship
 ----------------------------------------------
 TDDps_Hunter_Marksmanship = function()
-	local timeShift, spellName = TD_EndCast();
-	local gcd = TD_GlobalCooldown();
+	local timeShift, currentSpell, gcd = TD_EndCast();
 
-	local toh, tothCharges = TD_Aura(_ThrillOfTheHunt, timeShift);
-	local rf = TD_Aura(_RapidFire, timeShift);
+	local lol, lolCharges = TD_Aura(_LockandLoad, timeShift);
+	local ts = TD_Aura(_Trueshot, timeShift);
+	local hm = TD_TargetAura(_HuntersMark, timeShift);
+	local vul, vulCd = TD_TargetAura('Vulnerable', timeShift);
 
-	local stamp = TD_SpellAvailable(_Stampede, timeShift);
-	local chimaera, chimaeraCD = TD_SpellAvailable(_ChimaeraShot, timeShift);
-	local ks = TD_SpellAvailable(_KillShotMM, timeShift);
-	local rfCd = TD_SpellAvailable(_RapidFire, timeShift);
+	local amoc = TD_SpellAvailable(_AMurderOfCrows, timeShift);
+	local tsCd = TD_SpellAvailable(_Trueshot, timeShift);
+	local wb = TD_SpellAvailable(_Windburst, timeShift);
 	local barr = TD_SpellAvailable(_Barrage, timeShift);
 
-	local targetPH = TD_TargetPercentHealth();
-	local careful = rf or targetPH > 0.8;
-	local aimedShotCost = toh and _AimedShotCost or (_AimedShotCost - 20);
+	local aimedShotCost = _AimedShotCost;
+	if lol then
+		aimedShotCost = 0;
+	end
 
 	local minusFocus = 0;
-	if spellName == 'Aimed Shot' then
-		if careful then
-			-- almost certainly a crit, will refund a focus
-			minusFocus = aimedShotCost - 20;
-		else
-			minusFocus = aimedShotCost;
-		end
+	if currentSpell == 'Aimed Shot' then
+		minusFocus = 50;
 	end
-	if spellName == 'Steady Shot' then
-		minusFocus = -14;
+	if currentSpell == 'Windburst' then
+		minusFocus = 20;
 	end
-	if spellName == 'Focusing Shot' then
-		minusFocus = -50;
-	end
-
 	local focus, focusMax = TDDps_Hunter_Focus(minusFocus, timeShift);
 
-	if isStampede then
-		TDButton_GlowCooldown(_Stampede, stamp);
+	TDButton_GlowCooldown(_Trueshot, tsCd);
+	TDButton_GlowCooldown(_AMurderOfCrows, isMurderofcrows and amoc);
+
+	if hm and ((vul and vulCd < 2) or not vul) then
+		return _MarkedShot;
 	end
 
-	if isBarrage then
-		TDButton_GlowCooldown(_Barrage, barr);
+	if vul and (vulCd > 2 and lol) then
+		return _AimedShot;
 	end
 
-	TDButton_GlowCooldown(_RapidFire, rfCd);
-
-	-- Chimaera Shot on cooldown.
-	if chimaeraCD < gcd / 2 then
-		if focus < _ChimaeraShotCost then
-			return _SteadyShot;
-		else
-			return _ChimaeraShot;
-		end
+	if wb and currentSpell ~= 'Windburst' then
+		return _Windburst;
 	end
 
-	-- Kill Shot on cooldown if target below 35%
-	if targetPH < 0.35 and ks then
-		return _KillShotMM;
+	if focus <= aimedShotCost then
+		return isSidewinders and _Sidewinders or _ArcaneShot;
 	end
 
-	if careful and tothCharges > 0 and (focus >= aimedShotCost) then
+	if barr then
+		return _Barrage;
+	end
+
+	if vul and (vulCd > 2 or not hm) and focus > 40 then
 		return _AimedShot;
 	end
 
 	-- Aimed Shot to dump Focus.
-	if focus >= focusMax - 40 then
+	if focus >= focusMax - 10 then
 		return _AimedShot;
 	end
 
+	if focus <= _AimedShotCost - 10 then
+		return isSidewinders and _Sidewinders or _ArcaneShot;
+	end
+
 	-- If nothing else, Steady Shot
-	return _SteadyShot;
+	return isSidewinders and _Sidewinders or _ArcaneShot;
 end
 
 ----------------------------------------------
 -- Main rotation: Survival
 ----------------------------------------------
 TDDps_Hunter_Survival = function()
-
-	local timeShift, spellName = TD_EndCast();
-
-	local focus = TDDps_Hunter_Focus(0, timeShift);
-	if spellName == 'Cobra Shot' then
-		focus = focus + 14;
-	end
-
-	local _, focusRegen = GetPowerRegen();
-	local toh = TD_Aura(_ThrillOfTheHunt);
-
-	local stamp, stampCD = TD_SpellAvailable(_Stampede, timeShift);
-	local ss = TD_TargetAura(_SerpentSting, timeShift + 3);
-	local arcaneShotCost = toh and _ArcaneShotCost or (_ArcaneShotCost - 20);
-
-	-- 0. Stampede
-	if isStampede then
-		TDButton_GlowCooldown(_Stampede, stamp);
-	end
-
-	-- 0. A Murder of Crows
-	--	if  focus >= _AMurderOfCrowsCost and TD_SpellAvailable(_AMurderOfCrows) then
-	--		return _AMurderOfCrows;
-	--	end
-
-	-- 1. Explosive Shot
-	if focus >= _ExplosiveShotCost and TD_SpellAvailable(_ExplosiveShot, timeShift + 1) then
-		return _ExplosiveShot;
-	end
-
-	-- 2. Black Arrow
-	if TD_SpellAvailable(_BlackArrow, timeShift + 1) then
-		return _BlackArrow;
-	end
-
-	-- 3. Arcane Shot
-	if focus >= arcaneShotCost and not ss then
-		return _ArcaneShot;
-	end
-
-	-- 4. Barrage
-	if focus >= _BarrageCost and TD_SpellAvailable(_Barrage, timeShift + 1) then
-		return _Barrage;
-	end
-
-	-- 5. Barrage
-	if focus >= 70 then
-		return _ArcaneShot;
-	end
-
-	return _CobraShot;
+	return nil;
 end
 ----------------------------------------------
 -- Current or Future Focus
@@ -315,36 +238,5 @@ function TDDps_Hunter_Focus(minus, timeShift)
 		power = powerMax;
 	end;
 	return power, powerMax;
-end
-
-----------------------------------------------
--- Is Rapid Fire Available
-----------------------------------------------
-function TDDps_Hunter_RapidFire()
-	local _, _, _, _, _, _, expirationTime = UnitAura('player', 'Rapid Fire');
-	if expirationTime ~= nil and (expirationTime - GetTime()) > 0.2 then
-		return true;
-	end
-	return false;
-end
-
-
-----------------------------------------------
--- Is Steady Focus Available
-----------------------------------------------
-function TDDps_Hunter_SteadyFocus()
-	local _, _, _, _, _, _, expirationTime = UnitAura('player', 'Steady Focus');
-	if expirationTime ~= nil and (expirationTime - GetTime()) > 0.2 then
-		return true;
-	end
-	return false;
-end
-
-----------------------------------------------
--- Pet frenzy stacks count
-----------------------------------------------
-function TDDps_Hunter_PetFrenzy()
-	local _, _, _, count = UnitAura('pet', 'Frenzy')
-	return count or 0;
 end
 
